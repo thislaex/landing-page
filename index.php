5567b86a1515150bea523eccabe71f6feb573bb8
thislaex <51667211+thislaex@users.noreply.github.com>
2022-05-30 19:39:27 +0300

    Update
    Design updates have been made

--- index.php
+++ index.php
@@ -9,6 +9,8 @@
     <title>Landing Page</title>
     <meta name="keywords" content="laéx landing page">
     <meta name="description" content="laéx Landing Page">
+    <!-- JQUERY -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <!-- FONT GOOGLE -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -20,6 +22,10 @@
     <!-- TABLET -->
     <link rel="stylesheet" media="(max-width: 768px)" href="assets/css/tablet.css">
     <link rel="stylesheet" media="(max-width: 500px)" href="assets/css/mobile.css">
+
+    <!-- BOOTSTRAP -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
 </head>
 
 <body>
@@ -29,10 +35,10 @@
                 <h1 class="heading-small">laéx Landing Page</h1>
 
                 <ul>
-                    <li><a href="#home">Home</a></li>
-                    <li><a href="#projects">Projects</a></li>
-                    <li><a href="#mypersoneldevelopment">My Personal Development</a></li>
-                    <li><a href="#contact">Contact</a></li>
+                    <li class="nav-item"><a class="" href="#home">Home</a></li>
+                    <li class="nav-item"><a href="#projects">Projects</a></li>
+                    <li class="nav-item"><a href="#mypersoneldevelopment">My Personal Development</a></li>
+                    <li class="nav-item"><a href="#contact">Contact</a></li>
                 </ul>
             </nav>
         </div>
@@ -165,6 +171,10 @@
         </div>
 
     </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    <script src="assets/js/js.js"></script>
 </body>
 
 </html>
